f9a85127a03f54cf41800bfe6b8334ebcde7b10c
christophka <chris@ckapp.net>
2021-04-27 16:41:26 +0200

    docs(rxjs-node-process): update docs

--- packages/process/src/operators/skip-on-platforms.operator.ts
+++ packages/process/src/operators/skip-on-platforms.operator.ts
@@ -4,6 +4,16 @@ import { takeUntil } from 'rxjs/operators';
 import { platform$ } from '../platform';
 import { filterOnPlatforms } from './filter-on-platforms.operator';
 
+/**
+ * Emits values if the current node.js process doesn't run on one of
+ * the given `platforms`.
+ *
+ * @param platforms Platforms that are not allowed
+ *
+ * @returns
+ * Observable stream that emits if not on one of the given platforms.
+ * If on one of the given platforms, the observable completes.
+ */
 export function skipOnPlatforms<T>(
   platforms: NodeJS.Platform[],
 ): MonoTypeOperatorFunction<T> {
